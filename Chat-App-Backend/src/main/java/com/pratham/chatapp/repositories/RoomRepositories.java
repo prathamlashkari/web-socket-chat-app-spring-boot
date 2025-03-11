8f9cb0e5f3c21799350d0f5b15a805ae4dccf382
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-11 22:51:46 +0530

    created added method to find room in repo

--- Chat-App-Backend/src/main/java/com/pratham/chatapp/repositories/RoomRepositories.java
+++ Chat-App-Backend/src/main/java/com/pratham/chatapp/repositories/RoomRepositories.java
@@ -1,11 +1,10 @@
 package com.pratham.chatapp.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pratham.chatapp.models.Room;
 
-@Repository
 public interface RoomRepositories extends MongoRepository<Room, String> {
 
+  Room findByRoomId(String roomId);
 }
