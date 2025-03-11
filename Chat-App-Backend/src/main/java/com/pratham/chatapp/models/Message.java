aa0e53bba23185ce476ab677beb0e9d9efc2dbc6
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-11 22:29:44 +0530

    created message model

--- Chat-App-Backend/src/main/java/com/pratham/chatapp/models/Message.java
+++ Chat-App-Backend/src/main/java/com/pratham/chatapp/models/Message.java
@@ -1,5 +1,21 @@
 package com.pratham.chatapp.models;
 
-public class Message {
+import java.time.LocalDateTime;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "Message")
+public class Message {
+  private String sender;
+  private String content;
+  private LocalDateTime timeStamp;
+
+  public Message(String sender, String content) {
+    this.sender = sender;
+    this.content = content;
+    this.timeStamp = LocalDateTime.now();
+  }
 }
