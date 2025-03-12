dc8503a70a91a7f55a383edded85e9fb5681992f
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-12 21:25:36 +0530

    created api to get message for api

--- Chat-App-Backend/src/main/java/com/pratham/chatapp/controller/RoomController.java
+++ Chat-App-Backend/src/main/java/com/pratham/chatapp/controller/RoomController.java
@@ -1,5 +1,7 @@
 package com.pratham.chatapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pratham.chatapp.models.Message;
 import com.pratham.chatapp.models.Room;
 import com.pratham.chatapp.repositories.RoomRepositories;
 
@@ -37,6 +41,23 @@ public class RoomController {
     if (room == null)
       return new ResponseEntity<>("Room not found", HttpStatus.BAD_REQUEST);
     return new ResponseEntity<>(room, HttpStatus.FOUND);
+  }
+
+  @GetMapping("/{roomId}/messages")
+  public ResponseEntity<List<Message>> getMessages(
+      @PathVariable String roomId,
+      @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+      @RequestParam(value = "size", defaultValue = "20", required = false) int size) {
+    Room room = repositories.findByRoomId(roomId);
+    if (room == null) {
+      return ResponseEntity.badRequest().build();
+    }
+    List<Message> messages = room.getMessages();
+    int start = Math.max(0, messages.size() - (page + 1) * size);
+    int end = Math.min(messages.size(), start + size);
+    List<Message> paginatedMessages = messages.subList(start, end);
+    return ResponseEntity.ok(paginatedMessages);
+
   }
 
 }
