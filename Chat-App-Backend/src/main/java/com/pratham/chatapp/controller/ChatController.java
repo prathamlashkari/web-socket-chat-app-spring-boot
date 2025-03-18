464e3fa8f8539b7f7854b9cbddc297304a3160d2
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-18 21:45:56 +0530

    creating chat controller

--- Chat-App-Backend/src/main/java/com/pratham/chatapp/controller/ChatController.java
+++ Chat-App-Backend/src/main/java/com/pratham/chatapp/controller/ChatController.java
@@ -1,8 +1,14 @@
 package com.pratham.chatapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.pratham.chatapp.repositories.RoomRepositories;
 
 @Controller
 public class ChatController {
+
+  @Autowired
+  private RoomRepositories repositories;
 
 }
