507a266a947091dd7ccb6b087775156dae3a31f4
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-21 22:28:56 +0530

    created main page

--- Chat-app-FrontEnd/src/component/ChatPage.jsx
+++ Chat-app-FrontEnd/src/component/ChatPage.jsx
@@ -29,6 +29,40 @@ const ChatPage = () => {
           </button>
         </div>
       </header>
+      <main
+        ref={chatBoxRef}
+        className="py-20 px-10   w-2/3 dark:bg-slate-600 mx-auto h-screen overflow-auto "
+      >
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              message.sender === currentUser ? "justify-end" : "justify-start"
+            } `}
+          >
+            <div
+              className={`my-2 ${
+                message.sender === currentUser ? "bg-green-800" : "bg-gray-800"
+              } p-2 max-w-xs rounded`}
+            >
+              <div className="flex flex-row gap-2">
+                <img
+                  className="h-10 w-10"
+                  src={"https://avatar.iran.liara.run/public/43"}
+                  alt=""
+                />
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm font-bold">{message.sender}</p>
+                  <p>{message.content}</p>
+                  <p className="text-xs text-gray-400">
+                    {timeAgo(message.timeStamp)}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </main>
     </div>
   );
 };
