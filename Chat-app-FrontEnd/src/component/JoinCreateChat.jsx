4d00a80685cbedddbabcde0709de187e5730ebdf
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-21 20:48:01 +0530

    now adding functions one by one

--- Chat-app-FrontEnd/src/component/JoinCreateChat.jsx
+++ Chat-app-FrontEnd/src/component/JoinCreateChat.jsx
@@ -1,6 +1,81 @@
-import React from "react";
-
+import React, { useState } from "react";
+// import chatIcon from "../assets/chat.png";
+import toast from "react-hot-toast";
+// import { createRoomApi, joinChatApi } from "../services/RoomService";
+// import useChatContext from "../context/ChatContext";
+import { useNavigate } from "react-router";
 const JoinCreateChat = () => {
+  const [detail, setDetail] = useState({
+    roomId: "",
+    userName: "",
+  });
+
+  const { roomId, userName, setRoomId, setCurrentUser, setConnected } =
+    useChatContext();
+  const navigate = useNavigate();
+
+  function handleFormInputChange(event) {
+    setDetail({
+      ...detail,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function validateForm() {
+    if (detail.roomId === "" || detail.userName === "") {
+      toast.error("Invalid Input !!");
+      return false;
+    }
+    return true;
+  }
+
+  async function joinChat() {
+    if (validateForm()) {
+      try {
+        const room = await joinChatApi(detail.roomId);
+        toast.success("joined..");
+        setCurrentUser(detail.userName);
+        setRoomId(room.roomId);
+        setConnected(true);
+        navigate("/chat");
+      } catch (error) {
+        if (error.status == 400) {
+          toast.error(error.response.data);
+        } else {
+          toast.error("Error in joining room");
+        }
+        console.log(error);
+      }
+    }
+  }
+
+  async function createRoom() {
+    if (validateForm()) {
+      //create room
+      console.log(detail);
+      // call api to create room on backend
+      try {
+        const response = await createRoomApi(detail.roomId);
+        console.log(response);
+        toast.success("Room Created Successfully !!");
+        //join the room
+        setCurrentUser(detail.userName);
+        setRoomId(response.roomId);
+        setConnected(true);
+
+        navigate("/chat");
+
+        //forward to chat page...
+      } catch (error) {
+        console.log(error);
+        if (error.status == 400) {
+          toast.error("Room  already exists !!");
+        } else {
+          toast("Error in creating room");
+        }
+      }
+    }
+  }
   return (
     <div className="min-h-screen flex items-center justify-center ">
       <div className="p-10 dark:border-gray-700 border w-full flex flex-col gap-5 max-w-md rounded dark:bg-gray-900 shadow">
