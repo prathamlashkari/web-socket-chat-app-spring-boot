b169769b98b9fd803905c891f0d5a86fe76dd654
prathamlashkari <prathamlashkari841@gmail.com>
2025-03-29 23:22:37 +0530

    fixed all issue

--- Chat-app-FrontEnd/src/config/Router.jsx
+++ Chat-app-FrontEnd/src/config/Router.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import App from "../App.jsx";
-import JoinCreateChat from "../component/JoinCreateChat.jsx";
 import ChatPage from "../component/ChatPage.jsx";
-const Router = () => {
+const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<JoinCreateChat />} />
+      <Route path="/" element={<App />} />
       <Route path="/chat" element={<ChatPage />} />
     </Routes>
   );
 };
 
-export default Router;
+export default AppRoutes;
--- Chat-app-FrontEnd/src/main.jsx
+++ Chat-app-FrontEnd/src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
 import Router from "./config/Router.jsx";
+import AppRoutes from "./config/Router.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Router />
       <Toaster position="top-center" />
+      <ChatProvider>
+        <AppRoutes />
+      </ChatProvider>
     </BrowserRouter>
   </StrictMode>
 );
