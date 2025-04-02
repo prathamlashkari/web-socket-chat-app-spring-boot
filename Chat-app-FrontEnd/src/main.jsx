bba66d5bd6e2d040bf474b3c850cbca7d7f65cf7
prathamlashkari <prathamlashkari841@gmail.com>
2025-04-02 21:42:02 +0530

    now adding join api

--- Chat-app-FrontEnd/src/main.jsx
+++ Chat-app-FrontEnd/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
-import Router from "./config/Router.jsx";
 import AppRoutes from "./config/Router.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 
